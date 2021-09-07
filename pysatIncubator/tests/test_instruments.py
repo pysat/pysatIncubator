daa4850dba83f4c360dd8460c721dfa937d8586d
Jeff Klenzing <jklenzing@gmail.com>
2021-09-07 14:01:07 -0400

    DOC: docstrings and style in tests

--- pysatIncubator/tests/test_instruments.py
+++ pysatIncubator/tests/test_instruments.py
@@ -1,3 +1,11 @@
+"""Unit and Integration Tests for each instrument module.
+
+Note
+----
+Imports test methods from pysat.tests.instrument_test_class
+
+"""
+
 import tempfile
 
 import pytest
@@ -40,9 +48,19 @@ for method in method_list:
 
 
 class TestInstruments(InstTestClass):
+    """Main class for instrument tests.
+
+    Note
+    ----
+    Uses class level setup and teardown so that all tests use the same
+    temporary directory. We do not want to geneate a new tempdir for each test,
+    as the load tests need to be the same as the download tests.
+
+    """
 
     def setup_class(self):
-        """Runs once before the tests to initialize the testing setup."""
+        """Initialize the testing setup once before all tests are run."""
+
         # Make sure to use a temporary directory so that the user's setup is not
         # altered
         self.tempdir = tempfile.TemporaryDirectory()
@@ -52,15 +70,12 @@ class TestInstruments(InstTestClass):
         # to point to their own subpackage location, e.g.,
         # self.inst_loc = mypackage.instruments
         self.inst_loc = pysatIncubator.instruments
+        return
 
     def teardown_class(self):
-        """Runs after every method to clean up previous testing."""
+        """Clean up downloaded files and parameters from tests."""
+
         pysat.params.data['data_dirs'] = self.saved_path
         self.tempdir.cleanup()
-        del self.inst_loc, self.saved_path, self.tempdir
-
-    def setup_method(self):
-        """Runs before every method to create a clean testing setup."""
-
-    def teardown_method(self):
-        """Runs after every method to clean up previous testing."""
+        del self.inst_loc, self.saved_path, self.tempdir, self.stime
+        return
